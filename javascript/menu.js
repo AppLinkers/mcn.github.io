efc504be27681325a2aa156771e3ff409801c08d
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-08-31 14:18:24 +0900

    main page

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,6 +1,15 @@
+//change navbar styles on scroll
+
+window.addEventListener('scroll', ()=> {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+//click menu button
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 const closeBtn = document.getElementById("close");
+
 
 menuBtn.addEventListener('click',()=>{menu.classList.add("active")});
 closeBtn.addEventListener('click',()=>{menu.classList.remove("active")});
